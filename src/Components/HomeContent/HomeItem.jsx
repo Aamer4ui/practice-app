ccbdc5a81693e0547357dc866853c5e2e8c02497
Aamer4ui <aamersohelansari@gmail.com>
2021-01-29 00:01:25 +0530

    adding images alignment

--- src/Components/HomeContent/HomeItem.jsx
+++ src/Components/HomeContent/HomeItem.jsx
@@ -7,36 +7,13 @@ export const HomeItem = () => {
     <div>
       <Carousel>
         <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="homepage1.jpg"
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-          </Carousel.Caption>
+          <img className="d-block" src="pic1.jpg" alt="First slide" />
         </Carousel.Item>
         <Carousel.Item>
-          <img className="d-block w-50" src="HomePage.jpg" alt="Third slide" />
-
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
+          <img className="d-block" src="pic2.jpg" alt="Second slide" />
         </Carousel.Item>
         <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Third slide&bg=20232a"
-            alt="Third slide"
-          />
-
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
+          <img className="d-block" src="pic3.jpg" alt="Third slide" />
         </Carousel.Item>
       </Carousel>
       <p>
