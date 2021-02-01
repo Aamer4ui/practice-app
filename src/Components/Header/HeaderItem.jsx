dd11c4936eb0c2d47c638f3056bfd0083c9ff9ed
Aamer4ui <aamersohelansari@gmail.com>
2021-02-01 17:47:19 +0530

    change header

--- src/Components/Header/HeaderItem.jsx
+++ src/Components/Header/HeaderItem.jsx
@@ -5,9 +5,6 @@ export const HeaderItem = () => {
   return (
     <div>
       <header>
-        <a href="#" className="logo1">
-          <img src="c-logo-new.png" alt="univercity logo" />
-        </a>
         <a href="#" className="logo-name">
           <img src="logo-name.png" alt="" />
         </a>
